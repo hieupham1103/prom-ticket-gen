45b11d79ed1c3fd25ed39fdb3c64a9a4da0a625d
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2024-05-14 13:13:00 +0700

    oke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import shutil
 from PIL import ImageOps, ImageDraw, ImageFont
 from barcode import Code128
@@ -43,20 +44,45 @@ def make_qrcode(id, code):
     img.save(path)
     print(f"{id} done!!")
 
+def check_dup(file_path = "khach.csv"):
+    store_id = {}
+    store_code = {}
+    try:
+        file = open(file_path, mode = 'r', encoding = "utf-8-sig")    
+    except:
+        print("DEO CO FILE, DM THANG NGU")
+    
+    data = csv.reader(file)
+    for row in data:
+        if len(row) > 1:
+            if store_id.get(row[0]):
+                print(f"TRUNG ID: {row[0]}")
+            if store_code.get(row[1]):
+                print(f"TRUNG CODE: {row[1]}")
 
-def generate_guests(number, type = 0):
+            store_id[row[0]] = True
+            store_code[row[1]] = True
+            
+    print("DONE")
+
+def generate_guests(start_id, end_id, type = 0):
+    if os.path.isdir(OUTPUT_PATH) == False:
+        os.mkdir(OUTPUT_PATH)
     if type == 1:
-        for root, dirs, files in os.walk(OUTPUT_PATH):
-            for f in files:
-                os.unlink(os.path.join(root, f))
-            for d in dirs:
-                shutil.rmtree(os.path.join(root, d))
+        try:
+            for root, dirs, files in os.walk(OUTPUT_PATH):
+                for f in files:
+                    os.unlink(os.path.join(root, f))
+                for d in dirs:
+                    shutil.rmtree(os.path.join(root, d))
+        except:
+            pass
                 
     store_id = {}
     
     with open('khach.csv', mode='w') as khach:
         khach_writer = csv.writer(khach, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for id in range(number):
+        for id in range(start_id, end_id + 1):
             code = str(uuid.uuid4())[0:7]
             while store_id.get(code):
                 print("same id, gen again")
@@ -64,5 +90,39 @@ def generate_guests(number, type = 0):
             make_qrcode(id, code)
             khach_writer.writerow([id, f"\"{code}\""])
             store_id[code] = True
+    
+    check_dup()
 
-generate_guests(600, 1)
+
+
+if __name__ == "__main__":
+    print("Số id bắt đầu:")
+    start_id = input()
+    while True:
+        try:
+            start_id = int(start_id)
+            break
+        except:
+            print("Thử lại, số id bắt đầu:")
+            start_id = input()
+    print("Số id kết thúc:")
+    end_id = input()
+    while True:
+        try:
+            end_id = int(end_id)
+            break
+        except:
+            print("Thử lại, số id kết thúc:")
+            end_id = input()
+    print("Bạn có muốn xóa sạch file đã cho có không (0: không; 1: có):")
+    type = input()
+    while True:
+        try:
+            type = int(type)
+            break
+        except:
+            print("Thử lại, bạn có muốn xóa sạch file đã cho có không (0: không; 1: có):")
+            type = input()
+    
+    generate_guests(start_id, end_id, type)
+    
