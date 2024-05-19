aadf06bcdeb5a9446d73ae927da2287eb2c3d772
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2024-05-19 23:26:21 +0700

    oke:

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import shutil
-from PIL import ImageOps, ImageDraw, ImageFont
+from PIL import ImageOps, ImageDraw, ImageFont, Image
 from barcode import Code128
 from barcode.writer import ImageWriter 
 import csv
@@ -14,34 +14,32 @@ def make_qrcode(id, code):
     print(f"==Generate {id} - {code}==")
     code_img = Code128(code, writer=ImageWriter()).render()
     
-    # Add id number
-    expand_value = 30
-    img = ImageOps.expand(code_img, border = expand_value, fill = "white") 
-    width, height = img.size
-    img = img.crop((expand_value, expand_value, width - expand_value, height))
-    width, height = img.size
-    
-    
-    if img.mode != "RGB":
-        img = img.convert("RGB")
+    if code_img.mode != "RGBA":
+        code_img = code_img.convert("RGBA")
 
+    width, height = code_img.size
+    code_img = code_img.crop((0,100, width, height))
+    expand_value = 10
+    code_img = ImageOps.expand(code_img, border = expand_value, fill = "white") 
+    width, height = code_img.size
+    code_img = code_img.crop((25, 0, width - 20, height - 40))
+    width, height = code_img.size
+    code_img = code_img.resize((round(1.8 * width), round(1.8 * height)))
     
-    font = ImageFont.truetype("./font/RedditSans-VariableFont_wght.ttf",50)
-
-    draw = ImageDraw.Draw(img)
-    w = draw.textlength(id, font = font)
-    draw.text(
-        ((width-w)/2, height - 65),
-        id,
-        fill=(0, 0, 0),
-        font = font
-    )
+    
     path = OUTPUT_PATH + f"{id}.png"
     if os.path.isfile(path):
         print("File đã có")
         return
-        
+    
+    img = Image.open("./template.png").convert("RGBA")
+    
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("./font/RedditSans-VariableFont_wght.ttf",44)
+    draw.text((1100,75),id,font = font, fill = (0,0,0,255))
+    img.paste(code_img,(800,1680),code_img)
     img.save(path)
+    # img.show()
     print(f"{id} done!!")
 
 def check_dup(file_path = "khach.csv"):
@@ -81,7 +79,7 @@ def generate_guests(start_id, end_id, type = 0):
     store_id = {}
     
     with open('khach.csv', mode='w') as khach:
-        khach_writer = csv.writer(khach, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        khach_writer = csv.writer(khach, delimiter=',', lineterminator = '\n')
         for id in range(start_id, end_id + 1):
             code = str(uuid.uuid4())[0:7]
             while store_id.get(code):
@@ -96,33 +94,34 @@ def generate_guests(start_id, end_id, type = 0):
 
 
 if __name__ == "__main__":
-    print("Số id bắt đầu:")
-    start_id = input()
-    while True:
-        try:
-            start_id = int(start_id)
-            break
-        except:
-            print("Thử lại, số id bắt đầu:")
-            start_id = input()
-    print("Số id kết thúc:")
-    end_id = input()
-    while True:
-        try:
-            end_id = int(end_id)
-            break
-        except:
-            print("Thử lại, số id kết thúc:")
-            end_id = input()
-    print("Bạn có muốn xóa sạch file đã cho có không (0: không; 1: có):")
-    type = input()
-    while True:
-        try:
-            type = int(type)
-            break
-        except:
-            print("Thử lại, bạn có muốn xóa sạch file đã cho có không (0: không; 1: có):")
-            type = input()
+    # print("Số id bắt đầu:")
+    # start_id = input()
+    # while True:
+    #     try:
+    #         start_id = int(start_id)
+    #         break
+    #     except:
+    #         print("Thử lại, số id bắt đầu:")
+    #         start_id = input()
+    # print("Số id kết thúc:")
+    # end_id = input()
+    # while True:
+    #     try:
+    #         end_id = int(end_id)
+    #         break
+    #     except:
+    #         print("Thử lại, số id kết thúc:")
+    #         end_id = input()
+    # print("Bạn có muốn xóa sạch file đã cho có không (0: không; 1: có):")
+    # type = input()
+    # while True:
+    #     try:
+    #         type = int(type)
+    #         break
+    #     except:
+    #         print("Thử lại, bạn có muốn xóa sạch file đã cho có không (0: không; 1: có):")
+    #         type = input()
     
-    generate_guests(start_id, end_id, type)
+    # generate_guests(start_id, end_id, type)
+    generate_guests(0, 650, 1)
     
